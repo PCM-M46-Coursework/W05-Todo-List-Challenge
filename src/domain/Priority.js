136ae454947bbe0ef68079150920a277e1efdda5
ApacheTech Solutions <p_matthews@live.co.uk>
2023-03-31 21:28:57 +0100

    Add Domain Entities

---
+++ src/domain/Priority.js
@@ -0,0 +1,6 @@
+export default Priority = {
+    Critical: {  Level: 0, Title: "Critical" },
+    Important: {  Level: 1, Title: "Important" },
+    Normal: {  Level: 2, Title: "Normal" },
+    Low: {  Level: 3, Title: "Low" }
+};
